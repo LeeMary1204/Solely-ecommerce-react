9182a9009accf5e767949bfd9c31f4fbe9b2d401
LeeMary1204 <17351029217@163.com>
2023-08-16 05:03:43 -0700

    first commit

---
+++ src/services/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyATamX9ZzNmNHgyfCxUG1FLJcqaKLMGdkY",
+    authDomain: "ecommerce1-d3ea5.firebaseapp.com",
+    projectId: "ecommerce1-d3ea5",
+    storageBucket: "ecommerce1-d3ea5.appspot.com",
+    messagingSenderId: "256761661458",
+    appId: "1:256761661458:web:c3dc9b31d07b0a5e6ff0cb"
+};
+
+export default firebaseConfig;
